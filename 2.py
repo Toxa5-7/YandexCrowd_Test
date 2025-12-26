abcefb007165c39c2a94e650e82e4169550a9fbb
Anton <abaranov432@gmail.com>
2025-12-26 12:37:47 +0300

    сделана полностью

--- 2.py
+++ 2.py
@@ -1,15 +1,17 @@
 # Даны два строковых представления чисел A и B. Нужно максимизировать A, заменив в нём любую
 # цифру на цифру из B. Каждую цифру B можно использовать только один раз.
 
-A = "12345"
-B = "43536"
+A = "12355"
+B = "36"
 
 dA = [int(i) for i in A]
 dB = [int(i) for i in B]
 sort_dB = sorted(dB, reverse = True)
 
+pos_b = 0
 for i in range (len(A)):
-    if sort_dB[i]> dA[i]:
-        dA[i]=sort_dB[i]
+    if pos_b < len(dB) and sort_dB[pos_b]> dA[i]:
+        dA[i]=sort_dB[pos_b]
+        pos_b+=1
     
 print(dA)
